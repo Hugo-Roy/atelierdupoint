5f8e507278c6676a07cd4f0e12b313f7e4d1c6f1
Hugo-Roy <roy.hugo21@gmail.com>
2021-03-19 12:17:30 +0100

    site's done, only text last

--- wp-content/themes/atelierdupoint/front-page.php
+++ wp-content/themes/atelierdupoint/front-page.php
@@ -1,8 +1,10 @@
 <?php get_header() ?>
 <section class="banner">
-    
+    <h2 class="banner__title">Salome Baraer Tatouages</h2>
 </section>
-<main class="gallery">
-
+<main class="front">
+    <div class="front__container">
+        <img class="front__container__logo" src="<?= get_theme_file_uri('/assets/images/skull-home.svg') ?>" alt="le logo tête de mort de salon de tatouage l'atelier du point">
+    </div>
 </main>
 <?php get_footer() ?>
--- wp-content/themes/atelierdupoint/template-parts/footer/social-links.php
+++ wp-content/themes/atelierdupoint/template-parts/footer/social-links.php
@@ -1,4 +1,4 @@
 <div class="footer__social">
-    <a href="https://www.facebook.com/salome.baraer" class="footer__social__link"><i class="fab fa-facebook-square"></i></a>
+    <a href="https://www.facebook.com/Salom%C3%A9-Bara%C3%ABr-Tattoo-373699783177786" class="footer__social__link"><i class="fab fa-facebook-square"></i></a>
     <a href="https://www.instagram.com/salomebaraertattoo/?hl=fr" class="footer__social__link"><i class="fab fa-instagram-square"></i></a>
 </div>
